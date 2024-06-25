55fce24be56d7b65e5466d4a681825eaac9bbdc0
Flames <isaacomenuche@gmail.com>
2024-06-25 13:28:44 +0100

    ohayo

--- console.py
+++ console.py
@@ -145,8 +145,8 @@ class HBNBCommand(cmd.Cmd):
                 kwargs[key] = value
 
         new_instance = HBNBCommand.classes[model_class](**kwargs)
-        storage.save()
         storage.new(new_instance)
+        storage.save()
         print(new_instance.id)
 
     def help_create(self):
