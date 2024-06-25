d16bf1b5166923941db34a23a474869dab013ccb
Flames <isaacomenuche@gmail.com>
2024-06-25 03:10:08 +0100

    Update State

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy import Column, String
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+import models
+from models.city import City
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if models.storage_t == "db":
+        cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """ Getter for cities """
+            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
